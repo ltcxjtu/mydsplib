ebb0b264733a61af00e2a832f17b88e526ecab8e
ltcxjtu <1440935236@qq.com>
2017-06-26 20:43:31 +0800

    2016-06-26

--- particlefilter.m
+++ particlefilter.m
@@ -7,19 +7,42 @@ end
 
 function particles=predict(particles,deltaT,a,b)
 	%input:
-	%particles: a matrix about [x,y,dx,dy]
+	%particles: a matrix about [x,y,z,dx,dy,dz]
 	%deltaT: time interval
 	%	a:	Equation of motion parameter
 	%	b:	Equation of motion parameter
 	%output: new particles 
-	%alpha=[x,y,dx,dy];
+	%alpha=[x,y,z,dx,dy,dz];
 	%particles=[particle1 particle2 ...]
-	A=[1,0,a*deltaT,0;...
-		0,1,0,a*deltaT;...
-		0,0,a,0;...
-		0,0,0,a];
-	
+	%a=exp(-belta*deltaT);b=v*sqrt(1-a^2);
+	%Uk~N(mean,var);
+	A=[1,0,0,a*deltaT,0,0;...
+		0,1,0,a*deltaT,0;...
+		0,0,1,0,0,a*deltaT;...
+		0,0,0,a,0,0;...
+		0,0,0,0,a,0;...
+		0,0,0,0,0,a;];
+	B=[b*deltaT, 0,0;...
+		0,b*deltaT,0;...
+		0,0,b*deltaT;...
+		b,	0,	0;...
+		0,  b,	0;...
+		0,	0,	b];
+	MU=[0,0,0];
+	n=size(MU);
+	SIGMA=[1,0,0;...
+			0,1,0;...
+			0,0,1];
+	Uk = normrnd(MU,SIGMA,1,n);
+	particles=A*particles+B*Uk;
 
+	%normalize;
+	particles(:,1:3)=particles(1:3)./norm(particles(1:3));
+	particles(:,4:6)=particles(4:6)-particles(:,1:3)*(particles(:,4:6))'*particles(:,1:3);
+end
+
+function weights=update(particles,weights,Prob)
+	% Prob is the likelihood Pr(Zk|ak);to find my particles to the Prob
 
 
 end
